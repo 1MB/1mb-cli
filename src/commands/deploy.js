b2216c0ac54510b07dd984b7d75f65ddb6e9aa50
Jake Casto <jake@snaju.com>
2019-05-02 20:38:55 -0400

    Push v1.0.6

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -67,25 +67,105 @@ class DeployCommand extends Command {
 		})
 	}
 
-	const self = this
-    await glob.glob(flags.vuejs ? ['*.{html,css,js}', '*/*.{css,js}'] : '*.{html,css,js}', {cwd: process.cwd(), ignore: fs.existsSync(`${process.cwd()}/.gitignore`) ? parse(fs.readFileSync(`${process.cwd()}/.gitignore`)) : []}).then(files => {
-    	if(files.length === 0) {
-    		return self.log('Unable to find any html,css, or js files in your current directory')
-    	}
+	// check if pro before pulling files
+	request.post('https://api.1mb.site', {form: {
+		action: 'deploy',
+		site: username,
+		key: key,
+		resource: file,
+		code: minified
+	}}, async (error, response, body) => {
+		body = JSON.parse(body)
 
-    	cli.action.start(`Deploying ${files.length} files to 1mbsite..`)
+		let search = [];
+		if(body.error) {
+			search = flags.vuejs ? ['*.{html,css,js}', '*/*.{css,js}'] : '*.{html,css,js}';
+		}
+		else {
+			search = flags.vuejs ? ['*.{html,css,js,png,jpg,gif}', '*/*.{css,js,png,jpg,gif}'] : '*.{html,css,js,png,jpg,gif}'
+		}
 
-		for (var i = 0, len = files.length; i < len; i++) {
-			let file = files[i]
+		const self = this
+	    await glob.glob(search, {cwd: process.cwd(), ignore: fs.existsSync(`${process.cwd()}/.gitignore`) ? parse(fs.readFileSync(`${process.cwd()}/.gitignore`)) : []}).then(files => {
+	    	if(files.length === 0) {
+	    		return self.log('Unable to find any html,css, or js files in your current directory')
+	    	}
 
-			if(flags.minify) {
-				minify(`${process.cwd()}/${file}`).then(minified => {
+	    	cli.action.start(`Deploying ${files.length} files to 1mbsite..`)
+
+			for (var i = 0, len = files.length; i < len; i++) {
+				let file = files[i]
+
+				if(flags.minify) {
+					minify(`${process.cwd()}/${file}`).then(minified => {
+						request.post('https://api.1mb.site', {form: {
+							action: 'deploy',
+							site: username,
+							key: key,
+							resource: file,
+							code: minified
+						}}, function (error, response, body) {
+							body = JSON.parse(body)
+							
+						    if(body.error) {
+						        switch(body.error) {
+									case 'ACCOUNT_BANNED':
+										cli.action.stop('ERROR: Account banned.');
+									break;
+									case 'ACCOUNT_NONEXISTENT':
+										cli.action.stop('ERROR: Account doesn\'t exist.');
+									break;
+									case 'STORAGE_QUOTA':
+										cli.action.stop('ERROR: Account storage depleted.');
+									break;
+									case 'KEY_INCORRECT':
+										cli.action.stop('ERROR: Bad site key.');
+									break;
+									case 'EMAIL_VERIFICATION':
+										cli.action.stop('ERROR: Email not verified.');
+									break;
+									case 'KEY_INCLUDED':
+										cli.action.stop('ERROR: Site key found in code.');
+									break;
+									case 'RESOURCE_INVALID':
+										cli.action.stop('ERROR: Invalid file name.');
+									break;
+									case 'EXTENSION_INVALID':
+										cli.action.stop('ERROR: Unsupported file name extension.');
+									break;
+									case 'RESOURCE_LONG':
+										cli.action.stop('ERROR: File name too long.');
+									break;
+						        }
+						    }
+
+						    if(i === files.length && !body.error) {
+								cli.action.stop('Deployment successful!')
+
+							    notifier.notify({
+							      title: 'Deployment Successful!',
+							      message: 'We deployed the latest file versions to your website!'
+							    })
+						    }
+						})
+					})
+				}
+				else if(replace.length !== 0) {
+					var contents = fs.readFileSync(`${process.cwd()}/${file}`, 'utf8')
+					for (var n = 0, len = replace.length; n < len; n++) {
+						contents = contents.split(replace[n]).join('/')
+					}
+
+					for (var n1 = 0, len1 = images.length; n1 < len1; n1++) {
+						contents = contents.split(images[n1]['path']).join(images[n1]['url']).split(`/${images[n1]['path']}`).join(images[n1]['url'])
+					}
+
 					request.post('https://api.1mb.site', {form: {
 						action: 'deploy',
 						site: username,
 						key: key,
-						resource: file,
-						code: minified
+						resource: path.basename(file),
+						code: contents
 					}}, function (error, response, body) {
 						body = JSON.parse(body)
 						
@@ -130,124 +210,63 @@ class DeployCommand extends Command {
 						    })
 					    }
 					})
-				})
-			}
-			else if(replace.length !== 0) {
-				var contents = fs.readFileSync(`${process.cwd()}/${file}`, 'utf8')
-				for (var n = 0, len = replace.length; n < len; n++) {
-					contents = contents.split(replace[n]).join('/')
 				}
+				else {
+					request.post('https://api.1mb.site', {form: {
+						action: 'deploy',
+						site: username,
+						key: key,
+						resource: file,
+						code: fs.readFileSync(`${process.cwd()}/${file}`, 'utf8')
+					}}, function (error, response, body) {
+						body = JSON.parse(body)
+						
+					    if(body.error) {
 
-				for (var n1 = 0, len1 = images.length; n1 < len1; n1++) {
-					contents = contents.split(images[n1]['path']).join(images[n1]['url']).split(`/${images[n1]['path']}`).join(images[n1]['url'])
+					        switch(body.error) {
+								case 'ACCOUNT_BANNED':
+									cli.action.stop('ERROR: Account banned.');
+								break;
+								case 'ACCOUNT_NONEXISTENT':
+									cli.action.stop('ERROR: Account doesn\'t exist.');
+								break;
+								case 'STORAGE_QUOTA':
+									cli.action.stop('ERROR: Account storage depleted.');
+								break;
+								case 'KEY_INCORRECT':
+									cli.action.stop('ERROR: Bad site key.');
+								break;
+								case 'EMAIL_VERIFICATION':
+									cli.action.stop('ERROR: Email not verified.');
+								break;
+								case 'KEY_INCLUDED':
+									cli.action.stop('ERROR: Site key found in code.');
+								break;
+								case 'RESOURCE_INVALID':
+									cli.action.stop('ERROR: Invalid file name.');
+								break;
+								case 'EXTENSION_INVALID':
+									cli.action.stop('ERROR: Unsupported file name extension.');
+								break;
+								case 'RESOURCE_LONG':
+									cli.action.stop('ERROR: File name too long.');
+								break;
+					        }
+					    }
+
+					    if(i === files.length && !body.error) {
+							cli.action.stop('Deployment successful!')
+
+						    notifier.notify({
+						      title: 'Deployment Successful!',
+						      message: 'We deployed the latest file versions to your website!'
+						    })
+					    }
+					})
 				}
-
-				request.post('https://api.1mb.site', {form: {
-					action: 'deploy',
-					site: username,
-					key: key,
-					resource: path.basename(file),
-					code: contents
-				}}, function (error, response, body) {
-					body = JSON.parse(body)
-					
-				    if(body.error) {
-				        switch(body.error) {
-							case 'ACCOUNT_BANNED':
-								cli.action.stop('ERROR: Account banned.');
-							break;
-							case 'ACCOUNT_NONEXISTENT':
-								cli.action.stop('ERROR: Account doesn\'t exist.');
-							break;
-							case 'STORAGE_QUOTA':
-								cli.action.stop('ERROR: Account storage depleted.');
-							break;
-							case 'KEY_INCORRECT':
-								cli.action.stop('ERROR: Bad site key.');
-							break;
-							case 'EMAIL_VERIFICATION':
-								cli.action.stop('ERROR: Email not verified.');
-							break;
-							case 'KEY_INCLUDED':
-								cli.action.stop('ERROR: Site key found in code.');
-							break;
-							case 'RESOURCE_INVALID':
-								cli.action.stop('ERROR: Invalid file name.');
-							break;
-							case 'EXTENSION_INVALID':
-								cli.action.stop('ERROR: Unsupported file name extension.');
-							break;
-							case 'RESOURCE_LONG':
-								cli.action.stop('ERROR: File name too long.');
-							break;
-				        }
-				    }
-
-				    if(i === files.length && !body.error) {
-						cli.action.stop('Deployment successful!')
-
-					    notifier.notify({
-					      title: 'Deployment Successful!',
-					      message: 'We deployed the latest file versions to your website!'
-					    })
-				    }
-				})
 			}
-			else {
-				request.post('https://api.1mb.site', {form: {
-					action: 'deploy',
-					site: username,
-					key: key,
-					resource: file,
-					code: fs.readFileSync(`${process.cwd()}/${file}`, 'utf8')
-				}}, function (error, response, body) {
-					body = JSON.parse(body)
-					
-				    if(body.error) {
-
-				        switch(body.error) {
-							case 'ACCOUNT_BANNED':
-								cli.action.stop('ERROR: Account banned.');
-							break;
-							case 'ACCOUNT_NONEXISTENT':
-								cli.action.stop('ERROR: Account doesn\'t exist.');
-							break;
-							case 'STORAGE_QUOTA':
-								cli.action.stop('ERROR: Account storage depleted.');
-							break;
-							case 'KEY_INCORRECT':
-								cli.action.stop('ERROR: Bad site key.');
-							break;
-							case 'EMAIL_VERIFICATION':
-								cli.action.stop('ERROR: Email not verified.');
-							break;
-							case 'KEY_INCLUDED':
-								cli.action.stop('ERROR: Site key found in code.');
-							break;
-							case 'RESOURCE_INVALID':
-								cli.action.stop('ERROR: Invalid file name.');
-							break;
-							case 'EXTENSION_INVALID':
-								cli.action.stop('ERROR: Unsupported file name extension.');
-							break;
-							case 'RESOURCE_LONG':
-								cli.action.stop('ERROR: File name too long.');
-							break;
-				        }
-				    }
-
-				    if(i === files.length && !body.error) {
-						cli.action.stop('Deployment successful!')
-
-					    notifier.notify({
-					      title: 'Deployment Successful!',
-					      message: 'We deployed the latest file versions to your website!'
-					    })
-				    }
-				})
-			}
-		}
-    })
+	    })
+	})
   }
 }
 
